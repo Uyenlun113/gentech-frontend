4fb2b9c4c5d757d1b3c42049bcb0f64cecb5e6bb
Thiuyen113 <thiuyen1132004@gmail.com>
2025-07-16 23:54:12 +0700

    update

--- src/pages/Cash Capital/CashCapitalPage.jsx
+++ src/pages/Cash Capital/CashCapitalPage.jsx
@@ -30,7 +30,7 @@ export default function CashCapitalPage() {
     const handleIconClick = (type) => {
         switch (type) {
             case 'bank-receipt':
-                window.location.href = '/general-ledger/list';
+                window.location.href = '/chung-tu/phieu-thu';
                 break;
             case 'bank-payment':
                 window.location.href = '/phieu-xuat';
