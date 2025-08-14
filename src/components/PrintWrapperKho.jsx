0eb7ac966ea214440b5f18ff8eaf533a765d07c2
HiepnxDev <hiepnguyenxuan151@gmail.com>
2025-08-14 14:14:57 +0700

    update form trong kho

--- src/components/PrintWrapperKho.jsx
+++ src/components/PrintWrapperKho.jsx
@@ -16,10 +16,32 @@ const PrintWrapperKho = forwardRef(({
                 return printTemplatesKho.bang_ke_phieu_nhap;
             case 'bang_ke_phieu_nhap_mat_hang':
                 return printTemplatesKho.bang_ke_phieu_nhap_mat_hang;
-            case 'inventory-detail':
-                return printTemplatesKho.soTienGui;
-            case 'import-export-summary':
-                return printTemplatesKho.soChiTiet;
+            case 'bang_ke_phieu_nhap_mat_hang_ncc':
+                return printTemplatesKho.bang_ke_phieu_nhap_mat_hang_ncc;
+            case 'bang_ke_phieu_nhap_ncc_mat_hang':
+                return printTemplatesKho.bang_ke_phieu_nhap_ncc_mat_hang;
+            case 'tong_hop_hang_nhap_kho':
+                return printTemplatesKho.tong_hop_hang_nhap_kho;
+            case 'bang_ke_phieu_xuat':
+                return printTemplatesKho.bang_ke_phieu_xuat;
+            case 'bang_ke_phieu_xuat_mat_hang':
+                return printTemplatesKho.bang_ke_phieu_xuat_mat_hang;
+            case 'bang_ke_phieu_xuat_mat_hang_khach_hang':
+                return printTemplatesKho.bang_ke_phieu_xuat_mat_hang_khach_hang;
+            case 'bang_ke_phieu_xuat_khach_hang_mat_hang':
+                return printTemplatesKho.bang_ke_phieu_xuat_khach_hang_mat_hang;
+            case 'tong_hop_hang_xuat_kho':
+                return printTemplatesKho.tong_hop_hang_xuat_kho;
+            case 'the_kho_chi_tiet_vat_tu':
+                return printTemplatesKho.the_kho_chi_tiet_vat_tu;
+            case 'tong_hop_nhap_xuat_ton':
+                return printTemplatesKho.tong_hop_nhap_xuat_ton;
+            case 'tong_hop_nhap_xuat_ton_quy_doi':
+                return printTemplatesKho.tong_hop_nhap_xuat_ton_quy_doi;
+            case 'tong_hop_chi_tiet_vat_tu':
+                return printTemplatesKho.tong_hop_chi_tiet_vat_tu;
+            case 'bao_cao_ton_kho':
+                return printTemplatesKho.bao_cao_ton_kho;
             default:
                 return printTemplatesKho.default;
         }
